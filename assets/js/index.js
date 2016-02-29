1bb86fe2051e59dfd062f49ea06cba1f81d7e5c9
John Fox <john@classicyuppie.com>
2016-02-28 19:40:35 -0500

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        // On the home page, move the blog icon inside the header 
+        // for better relative/absolute positioning.
+
+        //$("#blog-logo").prependTo("#site-head-content");
+		$("#search").ghostHunter({
+              results: "#search-results",
+              zeroResultsInfo : false,
+              info_template   : "<p>Number of posts found: {{amount}}</p>",
+              onKeyUp         : true
+        });  
+    });
+
+}(jQuery));
+
